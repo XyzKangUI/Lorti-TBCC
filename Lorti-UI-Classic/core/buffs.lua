94e2e39504d2f2588d0fa5026fb08f316a8bcc7b
XyzKangUI <94811434+XyzKangUI@users.noreply.github.com>
2021-12-13 04:42:15 +0100

    Update buffs.lua

--- Lorti-UI-Classic/core/buffs.lua
+++ Lorti-UI-Classic/core/buffs.lua
@@ -3,6 +3,12 @@ local cfg = ns.cfg
 
 local function durationSetText(duration, arg1, arg2)
 	duration:SetText(format("|r"..string.gsub(arg1, " ", "").."|r", arg2))
+
+	if Lorti.switchtimer then
+		duration:SetPoint("BOTTOM", 0, -12)
+	else
+		duration:SetPoint("BOTTOM", 0, 0)
+	end
 end
 
 	--classcolor
@@ -43,7 +49,6 @@ end
   ---------------------------------------
 
 local function applySkin(b)
-	if not b or (b and b.styled) then return end
 	local name = b:GetName()
 	local tempenchant, debuff, buff = false, false, false
 
@@ -101,7 +106,6 @@ local function applySkin(b)
     	--duration
 	b.duration:SetFont(cfg.duration.font, cfg.duration.size, "THINOUTLINE")
 	b.duration:ClearAllPoints()
-	b.duration:SetPoint(cfg.duration.pos.a1,cfg.duration.pos.x,cfg.duration.pos.y)
 	hooksecurefunc(b.duration, "SetFormattedText", durationSetText)
 
     	--count
@@ -129,8 +133,8 @@ end
   ---------------------------------------
 
 for i = 1, NUM_TEMP_ENCHANT_FRAMES do
-	local button = _G['TempEnchant'..i]
-	applySkin(button)
+	local button = _G["TempEnchant"..i]
+	if (button and not button.styled) then applySkin(button) end
 end
 
 hooksecurefunc("AuraButton_Update", function(self, index)
